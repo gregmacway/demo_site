256c7500c818c17bec53ab64ffc6fc8675c2a83f
Greg Macway <greg.macway@gmail.com>
2014-05-04 18:44:42 -0700

    Finish user signup

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@
 
 
 DemoSite::Application.routes.draw do
-  get 'users/new'
+  resources :users
 
   root 'static_pages#home'
   match '/signup',  to: 'users#new',            via: 'get'
---
+++ spec/factories.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+  factory :user do
+    name     "Greg Macway"
+    email    "Greg.Macway@gmail.com"
+    password "aikoaiko"
+    password_confirmation "aikoaiko"
+  end
+end
